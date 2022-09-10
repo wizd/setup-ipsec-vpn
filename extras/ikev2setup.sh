1f4d8f3b832deadece53af496f5e1d9739834513
wizd <rcvbuf@gmail.com>
2022-09-11 04:31:36 +0800

    change ca name to hostname

--- extras/ikev2setup.sh
+++ extras/ikev2setup.sh
@@ -1469,7 +1469,7 @@ ikev2setup() {
     esac
   done
 
-  CA_NAME="IKEv2 VPN CA"
+  CA_NAME="IKEv2 VPN CA $(hostname -s)"
   CERT_DB="sql:/etc/ipsec.d"
   CONF_DIR="/etc/ipsec.d"
   CONF_FILE="/etc/ipsec.d/.vpnconfig"
